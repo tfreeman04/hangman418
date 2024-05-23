f6f8066bfa3bc2e3e67f46cfc6f0ead47b3cab7f
Tom Freeman <tfj1301@gmail.com>
2024-05-23 18:04:50 +0100

    task 2 complete checking for guess in word and validate guess

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,18 @@
+import random
+word_list = ["apple","orange","peach","pear","plum"]
+
+def choice (word_list):
+    return random.choice(word_list)
+word = choice(word_list)
+print(word)
 while True:
     guess = input("Please enter a letter: ")
     if len(guess) == 1 and guess.isalpha():
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character. ")
+
+if guess in word:
+    print(f"Good guess!{guess} is in the word")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
