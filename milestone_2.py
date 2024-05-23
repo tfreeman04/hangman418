20fa76e1cabe9ce9afe9d052b1e95bac916bf99d
Tom Freeman <tfj1301@gmail.com>
2024-05-23 17:34:17 +0100

    added user guess and check

--- milestone_2.py
+++ milestone_2.py
@@ -7,4 +7,11 @@ def choice (word_list):
     return random.choice(word_list)
 
 word = choice(word_list)
-print(word)
+# testing choice method 
+#print(word)
+guess = input("Please enter a single letter")
+if len(guess) == 1 and type(guess) == str:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
